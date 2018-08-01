c83f654d6fc3aff5b064e7b624b064c72bdf7919
wangxingchenyu <8279179133@qq.com>
2018-08-01 22:38:06 +0800

    React-手动实现low版本的redux

--- src/components/Vote/Body.js
+++ src/components/Vote/Body.js
@@ -2,26 +2,33 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
 export default class Body extends Component {
-    static contextTypes = {
-        n: PropTypes.number,
-        m: PropTypes.number
-    };
+
 
     constructor(props, context) {
         super(props);
-        console.log("aaaaaaaa");
-        console.log(context);
+        this.state = {refresh: false}
     }
 
+    //渲染完成后，执行did然后再随便定义一个setState,为了就是再次触发render
+    componentDidMount() {
+        this.props.myRedux.subscribe(() => {
+            this.setState({refresh: !this.state.refresh});  //this.setState()会自动触发render
+        })
+    }
+
+    /*
+    *改变后，自动执行
+    */
+
     render() {
-        let {n, m} = this.context;
-        let rate = n / (n + m);
-        isNaN(rate) ? rate = 0 : rate = rate * 100;
+        let {n = 0, m = 0} = this.props.myRedux.getState(), rate = n / (n + m)*100;
+        isNaN(rate) ? rate = 0 : null;
+        console.log(n, m);
         return (
             <div className='card-body'>
-                <div className='alert alert-success'>支持:{this.context.n}</div>
-                <div className='alert alert-danger'>反对:{this.context.m}</div>
-                <div className='alert alert-dark'>支持率:{(rate.toFixed(2)) + "%"}</div>
+                <div className='alert alert-success'>支持:{n}</div>
+                <div className='alert alert-danger'>反对:{m}</div>
+                <div className='alert alert-dark'>支持率:{rate.toFixed(2) + "%"}</div>
             </div>
         )
     }
