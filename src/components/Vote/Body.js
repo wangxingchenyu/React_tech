e4eb1e549831e03bf497c5e60671c357659c2d9d
wangxingchenyu <8279179133@qq.com>
2018-08-01 10:26:24 +0800

    React

--- src/components/Vote/Body.js
+++ src/components/Vote/Body.js
@@ -9,18 +9,19 @@ export default class Body extends Component {
 
     constructor(props, context) {
         super(props);
+        console.log("aaaaaaaa");
         console.log(context);
     }
 
     render() {
         let {n, m} = this.context;
         let rate = n / (n + m);
-        isNaN(rate) ? rate=0 : rate=rate*100;
+        isNaN(rate) ? rate = 0 : rate = rate * 100;
         return (
             <div className='card-body'>
                 <div className='alert alert-success'>支持:{this.context.n}</div>
                 <div className='alert alert-danger'>反对:{this.context.m}</div>
-                <div className='alert alert-dark'>支持率:{(rate.toFixed(2))+"%"}</div>
+                <div className='alert alert-dark'>支持率:{(rate.toFixed(2)) + "%"}</div>
             </div>
         )
     }
