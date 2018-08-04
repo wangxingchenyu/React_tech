83fe9fe84f2432691fda2262db371c06c6711c56
wangxingchenyu <8279179133@qq.com>
2018-08-04 13:55:26 +0800

    React-redux传递初始化数据

--- src/components/Vote/Body.js
+++ src/components/Vote/Body.js
@@ -1,26 +1,39 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import action from '../../store/action'
 
 export default class Body extends Component {
 
     constructor(props, context) {
         super(props);
-        this.state = {refresh: false}
+        this.state = {refresh: false};
+        let storeData = this.props.store.getState().vote;
+        this.state = {...storeData};
+    }
+
+    componentWillMount() {
+        this.props.store.dispatch(action.vote.init({
+            title: "标题",
+            content: "内容",
+            n: 0,
+            m: 0
+        }))
     }
 
     //渲染完成后，执行did然后再随便定义一个setState,为了就是再次触发render
     componentDidMount() {
         this.props.store.subscribe(() => {
             console.log("执行了Did");
-            this.setState({refresh: !this.state.refresh});  //this.setState()会自动触发render
+            this.setState({...this.props.store.getState().vote})
+            // this.setState({refresh: !this.state.refresh});  //this.setState()会自动触发render
         })
     }
 
     render() {
         console.log("执行了渲染");
-        let {n = 0, m = 0} = this.props.store.getState(), rate = n / (n + m) * 100;
+        console.log(this.state);
+        let {n, m} = this.state, rate = n / (n + m) * 100;
         isNaN(rate) ? rate = 0 : null;
-        console.log(n, m);
         return (
             <div className='card-body'>
                 <div className='alert alert-success'>支持:{n}</div>
